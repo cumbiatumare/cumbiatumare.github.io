dd17cb8dc6f6830036f0f878e8490517dc2cd854
fao1 <juan.ignacio.filardo@iis.fraunhofer.de>
2023-11-02 14:54:15 +0100

    chore: :truck: convert jpg images to webp

---
+++ converter.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Specify the directory where your JPG files are located
+jpg_directory="./docs/assets/"
+
+# Specify the directory to store the WebP files 
+webp_directory="./docs/assets/"
+
+# Loop through the JPG files and convert them to WebP
+for jpg_file in "$jpg_directory"*.jpg; do
+    filename=$(basename -- "$jpg_file")
+    filename_noext="${filename%.*}"
+    webp_file="$webp_directory${filename_noext}.webp"
+
+    # Convert the JPG to WebP using cwebp
+    cwebp "$jpg_file" -o "$webp_file"
+
+    echo "Converted: $jpg_file to $webp_file"
+done
